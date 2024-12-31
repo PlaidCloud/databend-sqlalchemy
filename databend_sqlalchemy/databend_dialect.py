020996e47ab40550d68580ab5e65a84aefdec0df
Pat Buxton <patrick.buxton@tartansolutions.com>
2024-12-31 17:00:14 +0000

    Support Interval Type

--- databend_sqlalchemy/databend_dialect.py
+++ databend_sqlalchemy/databend_dialect.py
@@ -57,6 +57,7 @@ from .dml import (
     Merge, StageClause, _StorageClause, GoogleCloudStorage,
     AzureBlobStorage, AmazonS3
 )
+from .types import INTERVAL
 
 RESERVED_WORDS = {
     'Error', 'EOI', 'Whitespace', 'Comment', 'CommentBlock', 'Ident', 'ColumnPosition', 'LiteralString',
@@ -238,44 +239,8 @@ class DatabendNumeric(sqltypes.Numeric):
         return process
 
 
-class DatabendInterval(sqltypes.Interval):
-    """Stores interval as a datetime relative to epoch, see base implementation."""
-
-    _reg = re.compile(r"(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)")
-
-    def result_processor(self, dialect, coltype):
-        def process(value):
-            if value is None:
-                return None
-            if isinstance(value, str):
-                m = self._reg.match(value)
-                if not m:
-                    raise ValueError(
-                        "could not parse %r as a datetime value" % (value,)
-                    )
-                groups = m.groups()
-                dt = datetime.datetime(*[
-                    int(groups[0] or self.epoch.year),
-                    int(groups[1] or self.epoch.month),
-                    int(groups[2] or self.epoch.day),
-                    int(groups[3] or 0),
-                    int(groups[4] or 0),
-                    int(groups[5] or 0),
-                ])
-            else:
-                dt = value
-            return dt - self.epoch
-
-        return process
-
-    def literal_processor(self, dialect):
-        def process(value):
-            if value is not None:
-                d = self.epoch + value
-                interval_str = d.isoformat(" ", timespec="microseconds")
-                return f"'{interval_str}'"
-
-        return process
+class DatabendInterval(INTERVAL):
+    render_bind_cast = True
 
 
 # Type converters
@@ -649,6 +614,9 @@ class DatabendTypeCompiler(compiler.GenericTypeCompiler):
 
     def visit_TIME(self, type_, **kw):
         return "DATETIME"
+
+    def visit_INTERVAL(self, type, **kw):
+        return "INTERVAL"
 
 
 class DatabendDDLCompiler(compiler.DDLCompiler):
