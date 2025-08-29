d3380439ac4358e2afac65bded6e803e7c08aaaf
Pat Buxton <patrick.buxton@tartansolutions.com>
2025-08-29 15:22:28 +0100

    Adjust default sequence_step_size

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,12 +14,10 @@ if version.parse(sqlalchemy.__version__) >= version.parse('2.0.0'):
     from sqlalchemy import event, text
     from sqlalchemy import Engine
 
-
     @event.listens_for(Engine, "connect")
     def receive_engine_connect(conn, r):
         cur = conn.cursor()
         cur.execute('SET global format_null_as_str = 0')
         cur.execute('SET global enable_geo_create_table = 1')
+        cur.execute("SET global sequence_step_size = 1")
         cur.close()
-
-
